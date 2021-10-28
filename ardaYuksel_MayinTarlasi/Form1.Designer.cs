bb9b7c9b683e38171338aff662cdef1ba8c53f20
yukselarda <91550253+yukselarda@users.noreply.github.com>
2021-10-28 18:22:23 +0300

    buton oluşturuldu ve mayin renkleri oluşturuldu

--- ardaYuksel_MayinTarlasi/Form1.Designer.cs
+++ ardaYuksel_MayinTarlasi/Form1.Designer.cs
@@ -29,13 +29,44 @@ namespace ardaYuksel_MayinTarlasi
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnUret = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(728, 452);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(734, 12);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(102, 57);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "Üret";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(850, 465);
+            this.Controls.Add(this.btnUret);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnUret;
     }
 }
 
--- ardaYuksel_MayinTarlasi/Form1.cs
+++ ardaYuksel_MayinTarlasi/Form1.cs
@@ -17,5 +17,48 @@ namespace ardaYuksel_MayinTarlasi
             InitializeComponent();
         }
 
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            List<int> mayinlar = new List<int>();
+            for (int i = 1; mayinlar.Count < 2; i++)
+            {
+                int sayi = rnd.Next(1, 65);
+                if (!mayinlar.Contains(sayi))
+                {
+                    mayinlar.Add(sayi);
+                }
+            }
+            for (int i = 1; i <= 64; i++)
+            {
+                Button btn = new Button();
+                btn.Text = i.ToString();
+                if (mayinlar.Contains(i))
+                {
+                    btn.Tag = true;
+                }
+                else
+                {
+                    btn.Tag = false; 
+                }
+                btn.Click += Btn_Click;
+                flowLayoutPanel1.Controls.Add(btn);
+
+            }
+        }
+
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button basilanbuton = (Button)sender;
+            bool mayin = (bool)basilanbuton.Tag;
+            if (mayin)
+            {
+                basilanbuton.BackColor = Color.Red;
+            }
+            else
+            {
+                basilanbuton.BackColor = Color.Green;
+            }
+        }
     }
 }
